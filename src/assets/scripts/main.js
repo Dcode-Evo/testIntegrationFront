9f19fa2a5574f20599327a5b3bf50d711df9cf2c
Guillaume W <guillaume.wilmotte@gmail.com>
2017-12-08 12:28:23 +0100

    creation de la modal

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('It works');
+var hotNewsItems = document.querySelectorAll('.hotNews-list__items a');
+
+for (let i = 0; i < hotNewsItems.length; i++) {
+  const element = hotNewsItems[i];
+  element.addEventListener('click', function(e) {
+    e.preventDefault();
+  })
+}
